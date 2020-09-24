eaa991129884761b862b2f290096ec8c5e5c0ba5
Ryan Korsak <rkorsak@gmail.com>
2020-09-24 18:38:12 -0400

    Modified Contest story to highlight use of templates and controls

--- src/components/Contest.stories.tsx
+++ src/components/Contest.stories.tsx
@@ -6,17 +6,33 @@ import { Contest } from './Contest';
 import * as election_description from '../mocks/description.json';
 import * as tally from '../mocks/tally.json';
 
+const contestIds = Object.keys(tally.contests);
+
 export default {
-    title: 'Examples/Contest',
-    component: Contest,
+    title: 'Contest',
+    // component: Contest,
+    argTypes: {
+        contestId: {
+            name: 'Contest ID',
+            control: { type: 'select', options: contestIds },
+        },
+    },
 } as Meta;
 
-export const Standard: Story = () => (
+interface StoryArgs {
+    contestId: string;
+}
+
+const Template: Story<StoryArgs> = ({ contestId }) => (
     <Contest
-        contest={tally.contests['congress-district-5-contest']}
-        description={
-            election_description.contests.filter((contest) => (contest.object_id = 'congress-district-5-contest'))[0]
-        }
+        contest={(tally as any).contests[contestId]}
+        description={election_description.contests.filter((contest) => contest.object_id === contestId)[0]}
         candidates={election_description.candidates}
     />
 );
+
+export const Standard = Template.bind({});
+Standard.storyName = 'Render Contest results';
+Standard.args = {
+    contestId: contestIds[0],
+};
