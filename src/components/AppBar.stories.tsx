03187d93b3456ad211e025cbacaf9244582dba7d
Keith Fung <keith.fung@infernored.com>
2020-10-01 19:52:50 -0400

    Remove using

--- src/components/AppBar.stories.tsx
+++ src/components/AppBar.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react/types-6-0';
 import AppBar, { AppBarProps } from './AppBar';
-import { PrimaryButton, Stack } from '@fluentui/react';
+import { Stack } from '@fluentui/react';
 import { useTheme } from '@fluentui/react-theme-provider';
 
 export default {
