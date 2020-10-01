e3f33440e7f5d123f74c9eac856e05a109282306
Keith Fung <keith.fung@infernored.com>
2020-10-01 19:44:40 -0400

    ✨ Details for Tracker Page

--- src/pages/InputTrackerPage.tsx
+++ src/pages/InputTrackerPage.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Text } from '@fluentui/react';
+import { Label, PrimaryButton, SearchBox } from '@fluentui/react';
 import ElectionHeader from '../components/ElectionTitle';
 import LargeCard from '../components/LargeCard';
+import { useHistory } from 'react-router-dom';
 
 const electionName = 'Mock Election';
 const startDate = '2020-03-01T08:00:00-05:00';
@@ -10,11 +11,21 @@ const endDate = '2020-10-01T08:00:00-05:00';
 export interface InputTrackerPageProps {}
 
 const InputTrackerPage: React.FunctionComponent<InputTrackerPageProps> = () => {
+    const history = useHistory();
+
+    const goToResults = () => {
+        history.push('/results');
+    };
     return (
         <>
             <ElectionHeader electionName={electionName} startDate={startDate} endDate={endDate} />
-            <LargeCard>
-                <Text as="h2">Tracking</Text>
+            <LargeCard alignToStart>
+                <Label>Ballot Search</Label>
+                <SearchBox styles={{ root: { width: '100%', maxWidth: 600 } }} placeholder="Input Ballot Tracker" />
+                <Label>Election Results</Label>
+                <PrimaryButton styles={{ root: { width: '100%', maxWidth: 150 } }} onClick={goToResults}>
+                    View
+                </PrimaryButton>
             </LargeCard>
         </>
     );
