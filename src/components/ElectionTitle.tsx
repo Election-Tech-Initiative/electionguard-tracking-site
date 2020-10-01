848033689c36c2551d2d3d8f295c152e67759399
Keith Fung <keith.fung@infernored.com>
2020-10-01 13:56:17 -0400

    ✨ Add Election Title

---
+++ src/components/ElectionTitle.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Text } from '@fluentui/react';
+import moment from 'moment';
+import { useTheme } from '@fluentui/react-theme-provider';
+import Title from './Title';
+
+// TODO #?? Resolve internalization of language using i18n
+const defaultElectionName = 'Election';
+// TODO #?? Resolve internalization of dates using i18n
+const dateFormat = 'MM/DD/YYYY';
+
+export interface ElectionTitleProps {
+    electionName?: string;
+    startDate: string;
+    endDate: string;
+}
+
+const ElectionTitle: React.FunctionComponent<ElectionTitleProps> = ({ electionName, startDate, endDate }) => {
+    const theme = useTheme();
+    return (
+        <Title title={electionName ?? defaultElectionName}>
+            <Text as="span" styles={{ root: { color: theme.palette.neutralSecondary, marginLeft: theme.spacing.l1 } }}>
+                {`${moment(startDate).format(dateFormat)}-${moment(endDate).format(dateFormat)}`}
+            </Text>
+        </Title>
+    );
+};
+
+export default ElectionTitle;
